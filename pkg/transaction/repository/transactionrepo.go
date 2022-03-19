5d968f17e900b0df7920f9e9da8009d877649380
TEAM <gullanianirudh1000@gmail.com>
2022-03-19 21:54:09 +0530

    fourth commit

--- pkg/transaction/repository/transactionrepo.go
+++ pkg/transaction/repository/transactionrepo.go
@@ -27,35 +27,35 @@ func StartTransaction(sender int64, receiver int64, amount int64) dao.Transactio
 	var tid int
 	txn := db.Transaction(func(tx *gorm.DB) error {
 		// do some database operations in the transaction (use 'tx' from this point, not 'db')
-		if err := db.Debug().Exec("INSERT INTO RT_TRANSACTION(TRANSACTION_UNIQUE_ID,TRANSACTION_TYPE_CODE,TRANSACTION_AMOUNT,CREATED_BY,UPDATED_BY)"+
+		if err := tx.Debug().Exec("INSERT INTO RT_TRANSACTION(TRANSACTION_UNIQUE_ID,TRANSACTION_TYPE_CODE,TRANSACTION_AMOUNT,CREATED_BY,UPDATED_BY)"+
 			" values(?,'b',?,'Admin','Admin');", uuid, amount).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
-		if err := db.Debug().Raw("select TRANSACTION_ID from RT_TRANSACTION where TRANSACTION_UNIQUE_ID = ?", uuid).Scan(&tid).Error; err != nil {
+		if err := tx.Debug().Raw("select TRANSACTION_ID from RT_TRANSACTION where TRANSACTION_UNIQUE_ID = ?", uuid).Scan(&tid).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
 		fmt.Println(tid)
-		if err := db.Debug().Exec("INSERT INTO RT_TRANSACTION_LEDGER(TRANSACTION_ID,ACCOUNT_ID,LEDGER_TYPE_CODE,LEDGER_TRANSACTION_AMOUNT,CREATED_BY"+
+		if err := tx.Debug().Exec("INSERT INTO RT_TRANSACTION_LEDGER(TRANSACTION_ID,ACCOUNT_ID,LEDGER_TYPE_CODE,LEDGER_TRANSACTION_AMOUNT,CREATED_BY"+
 			",UPDATED_BY) values (?,?,'D',?,'Admin','Admin');", tid, sender, -amount).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
-		if err := db.Debug().Exec("INSERT INTO RT_TRANSACTION_LEDGER(TRANSACTION_ID,ACCOUNT_ID,LEDGER_TYPE_CODE,LEDGER_TRANSACTION_AMOUNT,CREATED_BY"+
+		if err := tx.Debug().Exec("INSERT INTO RT_TRANSACTION_LEDGER(TRANSACTION_ID,ACCOUNT_ID,LEDGER_TYPE_CODE,LEDGER_TRANSACTION_AMOUNT,CREATED_BY"+
 			",UPDATED_BY) values (?,?,'C',?,'Admin','Admin');", tid, receiver, +amount).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
-		if err := db.Debug().Exec("UPDATE rm_account set MONEY_ACCOUNT_BALANCE = MONEY_ACCOUNT_BALANCE-? where ACCOUNT_ID=? ;", amount, sender).Error; err != nil {
+		if err := tx.Debug().Exec("UPDATE rm_account set MONEY_ACCOUNT_BALANCE = MONEY_ACCOUNT_BALANCE-? where ACCOUNT_ID=? ;", amount, sender).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
-		if err := db.Debug().Exec("UPDATE rm_account set MONEY_ACCOUNT_BALANCE = MONEY_ACCOUNT_BALANCE+? where ACCOUNT_ID=? ;", amount, receiver).Error; err != nil {
+		if err := tx.Debug().Exec("UPDATE rm_account set MONEY_ACCOUNT_BALANCE = MONEY_ACCOUNT_BALANCE+? where ACCOUNT_ID=? ;", amount, receiver).Error; err != nil {
 			fmt.Println(err)
 			return err
 		}
-		if err := db.Debug().Raw("select current_timestamp();").Scan(&result.Time).Error; err != nil {
+		if err := tx.Debug().Raw("select current_timestamp();").Scan(&result.Time).Error; err != nil {
 			fmt.Println(err, result.Time)
 			return err
 		}
--- pkg/transaction/services/services.go
+++ pkg/transaction/services/services.go
@@ -1,16 +1,21 @@
 package services
 
 import (
+	accountService "rpay/pkg/account/services"
 	"rpay/pkg/transaction/dao"
 	transactionRepo "rpay/pkg/transaction/repository"
 	userRepo "rpay/pkg/user/repository"
 )
 
 func StartTransaction(sender string, receiver string, amount int64) dao.Transaction_output {
-	senderPk := userRepo.GetUserAccountPk(sender)
-	receiverPk := userRepo.GetUserAccountPk(receiver)
-	result := transactionRepo.StartTransaction(senderPk, receiverPk, amount)
-	result.SenderName = sender
-	result.ReceiverName = receiver
-	return result
+	if accountService.GetBalanceByUid(sender) >= float64(amount) {
+		senderPk := userRepo.GetUserAccountPk(sender)
+		receiverPk := userRepo.GetUserAccountPk(receiver)
+		result := transactionRepo.StartTransaction(senderPk, receiverPk, amount)
+		result.SenderName = sender
+		result.ReceiverName = receiver
+		return result
+	} else {
+		return dao.Transaction_output{0, "", "", 0, sender, receiver}
+	}
 }
